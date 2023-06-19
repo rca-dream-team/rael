4eca385eb771ee5a78723bcb197518c1a0152f37
Valentin-Shyaka <vava1lantern7@gmail.com>
2023-06-19 14:58:40 +0200

    implemented the rca-daily page

--- src/app/rca-daily/page.tsx
+++ src/app/rca-daily/page.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
+import PText from '@/components/shared/PoppinText'
+import RText from '@/components/shared/RighteousText'
+
+
 const RcaDailyPage = () => {
-  return (
-    <div>RcaDailyPage</div>
-  )
+return (
+<div className=' w-[80%] '>
+  <RText className='text-3xl'>15th March</RText>
+  <div className='mt-5'>
+    <div className='border border-gray-800 rounded-xl w-full h-fit p-4'>
+      <RText className='text-xl'><h2>Nickman Football captain helps the team reach the peace cup finals</h2></RText>
+      <PText>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, autem facere atque cum in ipsa mollitia libero amet ratione. Quos enim a voluptatibus nobis voluptates neque accusamus repellendus doloremque</PText>
+    </div>
+  </div>
+</div>
+)
 }
 
 export default RcaDailyPage
