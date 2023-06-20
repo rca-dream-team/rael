65ec9d9dce3531604cacdbb00d10d2a6ffb8b5d1
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2023-06-20 11:22:47 +0200

    [feat]: member modal intersecting

--- src/app/members/[id]/member.tsx
+++ src/app/members/[id]/member.tsx
@@ -3,7 +3,7 @@ import RText from "@/components/shared/RighteousText";
 import React from "react";
 import Image from "next/image";
 
-interface MemberProps {
+export interface MemberProps {
   data: {
     image: string;
     title: string;
@@ -11,23 +11,28 @@ interface MemberProps {
     desc: string;
     bio: string;
     images?: string[];
-  };
+  } | null;
 }
 
 const Member = ({ data }: MemberProps) => {
   return (
     <div className="max-w-[1000px] w-full rounded-xl bg-white gap-y-11 px-11 flex-col flex">
-      <div className="flex items-center justify-between">
-        <Image src={data.image} height={300} width={300} alt="Member" />
+      <div className="flex items-center justify-center">
+        <Image
+          src={data?.image ?? "/images/member.png"}
+          height={300}
+          width={300}
+          alt="Member"
+        />
         <div className="flex flex-col gap-y-7 w-full px-11">
           <div className="flex flex-col">
-            <RText className=" text-xl uppercase">{data.name}</RText>
-            <PText className=" font-medium uppercase">{data.title}</PText>
+            <RText className=" text-xl uppercase">{data?.name}</RText>
+            <PText className=" font-medium uppercase">{data?.title}</PText>
           </div>
-          <PText className=" text-sm">{data.bio}</PText>
+          <PText className=" text-sm">{data?.bio}</PText>
         </div>
       </div>
-      <PText className="text-sm">{data.desc}</PText>
+      <PText className="text-sm">{data?.desc}</PText>
     </div>
   );
 };
--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,11 +1,24 @@
 import MainAppLayout from "@/components/layouts/MainAppLayout";
 import React from "react";
-import MemberModal from "./member-modal";
+import Image from "next/image";
+import Link from "next/link";
 
-const MembersPage = () => {
+const MembersPage = async () => {
   return (
     <MainAppLayout>
-      <div className=""></div>
+      <div className="grid grid-cols-4 w-full gap-4 mt-4">
+        {new Array(10).fill(0).map((_, i) => (
+          <Link key={i} href={`/members/${i + 1}`}>
+            <Image
+              src={"/images/mem3.png"}
+              width={300}
+              height={300}
+              className=" object-cover"
+              alt="member"
+            />
+          </Link>
+        ))}
+      </div>
     </MainAppLayout>
   );
 };
