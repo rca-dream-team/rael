0b0cce47a858d74ae3ff1b51265f33bcf1e2cc19
ndungutsecharles103 <ndungutse@kali.carimount>
2023-06-23 19:24:38 +0200

    [fix]: searchbar + header

--- client/src/components/constants/Header.tsx
+++ client/src/components/constants/Header.tsx
@@ -1,32 +1,67 @@
 "use client";
 import Image from "next/image";
-import React from "react";
+import React, { useRef } from "react";
 import { BellIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { RAELIcon } from "../shared/icons";
 import { useState } from "react";
-import SearchDiv from "../SearchDiv";
+import SearchBar from "./SearchBar";
+import { Popover } from "@mantine/core";
+import Link from "next/link";
 
 const Header = () => {
   const [searchVal, setSearchVal] = useState(false);
+
+  const handleShowSearch = () => {
+    setSearchVal(true);
+  };
+
   return (
     <div className="flex w-full dark:border-slate-500 items-center justify-between border-b-2 py-6">
       <div className=""></div>
       {/* <Image src={"/logo.svg"} height={40} width={100} alt="rael" /> */}
       <RAELIcon size={100} />
-      <div className="flex gap-x-3 items-center">
-        <MagnifyingGlassIcon
-          className="w-6 font-bold"
-          onClick={() => setSearchVal(!searchVal)}
-        />
-        {searchVal ? <SearchDiv /> : <></>}
-        <BellIcon className="w-6 font-bold" />
-        <Image
-          src={"/svgs/avatar.svg"}
-          alt="avatar"
-          height={40}
-          width={40}
-          className=" rounded-full object-cover"
-        />
+      <div className="flex gap-x-3 relative items-center">
+        <div className="flex items-center absolute right-20 gap-x-3">
+          <MagnifyingGlassIcon
+            className="w-6 font-bold cursor-pointer"
+            onClick={handleShowSearch}
+          />
+          {searchVal ? <SearchBar setSearchVal={setSearchVal} /> : <></>}
+        </div>
+        <BellIcon className="w-6 cursor-pointer font-bold" />
+        <Popover width={200} position="bottom" shadow="md">
+          <Popover.Target>
+            <Image
+              src={"/svgs/avatar.svg"}
+              alt="avatar"
+              height={40}
+              width={40}
+              className=" rounded-full cursor-pointer object-cover"
+            />
+          </Popover.Target>
+          <Popover.Dropdown p={0} className=" border-gray-700 overflow-hidden">
+            <div className="flex dark:bg-black flex-col py-2">
+              <Link
+                href={"#"}
+                className="text-sm font-poppins p-2 hover:text-white hover:bg-gray-600 font-bold"
+              >
+                Profile
+              </Link>
+              <Link
+                href={"#"}
+                className="text-sm font-poppins p-2 hover:text-white hover:bg-gray-600 font-bold"
+              >
+                Settings
+              </Link>
+              <Link
+                href={"#"}
+                className="text-sm font-poppins p-2 hover:text-white hover:bg-gray-600 font-bold"
+              >
+                Logout
+              </Link>
+            </div>
+          </Popover.Dropdown>
+        </Popover>
       </div>
     </div>
   );
