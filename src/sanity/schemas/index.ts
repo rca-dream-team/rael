04df4f4e6c2697ff56d01f8e0e7a6f6d13b7d319
ndungtse <ndungutsecharles103@gmail.com>
2024-03-11 21:27:36 +0200

    feat: request schema

--- src/sanity/schemas/index.ts
+++ src/sanity/schemas/index.ts
@@ -2,6 +2,7 @@ import author from './documents/author';
 import className from './documents/class';
 import maintainer from './documents/maintainer';
 import news from './documents/news';
+import profileRequest from './documents/profile-request';
 import project from './documents/project';
 import promotion from './documents/promotion';
 import staff from './documents/staff';
@@ -13,6 +14,6 @@ import link from './objects/link';
 import socials from './objects/socials';
 
 const objects = [blockContent, socials, link];
-const documents = [promotion, student, project, timeline, staff, staffRole, className, author, maintainer, news];
+const documents = [promotion, student, project, timeline, staff, staffRole, className, author, maintainer, news, profileRequest];
 
 export const schemaTypes = [...objects, ...documents];
