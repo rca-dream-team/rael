abd1cf2a0f4b21c34f27844f54cbc3141b907fa5
Valentin-Shyaka <vava1lantern7@gmail.com>
2024-03-16 18:17:11 +0200

    added index to footer ...

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -3,7 +3,7 @@ import PText from '../constants/PoppinText';
 
 const Footer = () => {
    return (
-      <div className="border-t-2 mt-auto flex items-center justify-center py-6 border-stone-500 w-full">
+      <div className="border-t-2 mt-auto flex items-center justify-center py-6 border-stone-500 w-full z-50">
          <PText noDark>Product of RCA #ClassOf2024</PText>
       </div>
    );
